530d47fc2e4cdd05702e4cc300b182d0185be963
Marcos Henrique de Melo <hmelo56@gmail.com>
2024-09-30 16:31:40 -0300

    Voltando a versão antiga

--- pages/index.js
+++ pages/index.js
@@ -2,58 +2,19 @@ import { useState } from "react"
 
 function Home() {
   return (
-    <div id="container">
-      <div id="profile">
-        <img
-          src="./assets/assets/avatar.png"
-          alt="Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
-        />
-        <p>@maykbrito</p>
-      </div>
-      <div id="switch" onclick="toggleMode()">
-        <button></button>
-        <span></span>
-      </div>
-      <ul>
-        <li>
-          <a href="#">Inscrever-se no NLW</a>
-        </li>
-        <li>
-          <a href="#">Baixar meu e-book</a>
-        </li>
-        <li>
-          <a href="https://github.com/maykbrito" target="_blank"
-            >Ver meu portifólio</a
-          >
-        </li>
-        <li>
-          <a href="https://rocketseat.com.br/explorer" target="_blank"
-            >Conheça o Explorer</a
-          >
-        </li>
-      </ul>
-      <div id="social-links">
-        <a href="https://github.com/hmelo56 " target="_blank"
-          ><ion-icon name="logo-github"></ion-icon
-        ></a>
-        <a href="https://instagram.com/hmelo56 " target="_blank"
-          ><ion-icon name="logo-instagram"></ion-icon
-        ></a>
-        <a href="https://youtube.com/MarcosHenriqueMelo " target="_blank"
-          ><ion-icon name="logo-youtube"></ion-icon
-        ></a>
-        <a
-          href="https://www.linkedin.com/in/marcos-henrique-de-melo-453832135/ "
-          target="_blank"
-          ><ion-icon name="logo-linkedin"></ion-icon
-        ></a>
-      </div>
-      <footer>
-        Feito com ♥ pela
-        <a href="https://rocketseat.com.br/" target="_blank">Rocketseat</a>
-      </footer>
+    <div>
+      <h1>Sejam Bem-Vindos!</h1>
+      <Contador />
+       <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="servicos.html">Serviços</a></li>
+        <li><a href="convenios.html">Convênios</a></li>
+        <li><a href="parcerias.html">Parcerias</a></li>
+        <li><a href="comite.html">Comitê</a></li>
+        <li><a href="contatos.html">Contatos</a></li>
+    </ul>
     </div>
-    )
+  )
 }
 
 function Contador(){
