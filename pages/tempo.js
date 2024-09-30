131f98eee9e522aeb94f3188a3b00941ff479050
Marcos Henrique de Melo <hmelo56@gmail.com>
2024-09-30 20:19:55 -0300

    Props estático

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,11 +1,19 @@
-function Tempo() {
+function Tempo(props) {
   const dynamicDate = new Date()
-  const dynamicDateString = dynamicDate.toGMTString();
-  return (
-    <div>
-      {dynamicDateString} (dinâmico)
-    </div>
-  )
+  const dynamicDateString = dynamicDate.toGMTString()
+  return {
+    <div>{dynamicDateString} (dinâmico)</div>
+    <div>{props.staticDateString} (estático)</div>
+}
+
+export function getStaticProps() {
+  const dynamicDate = new Date()
+  const dynamicDateString = dynamicDate.toGMTString()
+  return {
+    props: {
+      staticDateString
+    }
+  }
 }
 
 export default Tempo
