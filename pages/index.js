26bc480b3074aa184d7107bd8ddb006b2b851a9f
Marcos Henrique de Melo <hmelo56@gmail.com>
2024-09-30 19:37:04 -0300

    Configurando

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function Sobre() {
+function Home() {
   return (
     <div>
       <h1>Home</h1>
--- pages/sobre.js
+++ pages/sobre.js
@@ -4,21 +4,7 @@ function Sobre() {
   return (
     <div>
       <h1>Sobre</h1>
-      <Contador />
       <a href="/">Acessar página Home</a>
-    </div>
-  )
-}
-
-function Contador() {
-  const [contador, setContador] = useState(1)
-  function adcionarContador() {
-    setContador(contador + 1)
-  }
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adcionarContador}>Adiconar</button>
     </div>
   )
 }
