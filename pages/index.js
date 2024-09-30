b4cfb665f203f67dd3c283caae15966f87ec1a45
Marcos Henrique de Melo <hmelo56@gmail.com>
2024-09-30 19:51:52 -0300

    Linkando páginas

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import Link from "next/link"
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Contador />
-      <a href="/sobre">Acessar página Sobre</a>
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
     </div>
   )
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import Link from "next/link"
 
 function Sobre() {
   return (
     <div>
       <h1>Sobre</h1>
-      <a href="/">Acessar página Home</a>
+      <Link href="/">
+        <a>Acessar página Home</a>
+      </Link>
     </div>
   )
 }
